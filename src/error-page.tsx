45612d929b2da0c47bd8f678fd62ed7b938fbca0
jorgelucasalima <jorgelucasalima@gmail.com>
2024-10-09 15:53:08 -0300

    ajuste errorpage

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -1,16 +1,23 @@
+import { Flex, Link, Text } from "@radix-ui/themes";
 import { useRouteError } from "react-router-dom";
 
+interface RouteError {
+  statusText?: string;
+  message?: string;
+}
+
 export default function ErrorPage() {
-  const error = useRouteError();
+  const error = useRouteError() as RouteError;
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+    <div id="error-page" role="alert" className="radix-themes" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+      <Text size="5" weight="bold">Oops!</Text>
+      <p>Desculpe por isso, mas parece que algo deu errado.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>{error?.statusText || error?.message || "Unknown error"}</i>
       </p>
+      <Link href="/">Voltar</Link>
     </div>
   );
 }
